f53e40586ce13d1f9ddd63b274edcb52ed9662b5
M Fazeel Arif <arif.fazeel99@gmail.com>
2023-05-17 16:08:23 +0200

    updated pipeline transformation function

--- data/pipeline.py
+++ data/pipeline.py
@@ -4,8 +4,7 @@ from sqlalchemy import create_engine
 import numpy as np
 
 # Extract data from web
-Munster = pd.read_csv( \
-    'https://fazeelarif.com/autoscout24-germany-dataset.csv')
+Munster = pd.read_csv('https://fazeelarif.com/autoscout24-germany-dataset.csv')
 AutoScout24 = pd.read_excel('https://opendata.stadt-muenster.de/sites/default/files/Fahrzeugbestand-Regierungsbezirk-Muenster-2018-2022.xlsx')
 
 # Transformtion
@@ -16,8 +15,25 @@ Munster.replace(np.nan, 0)
 
 #####
 
+# Renaming the columns to english titles
+AutoScout24.rename(
+    columns={
+        "Jahr": "Year",
+        "unbe-kannt": "Unknown",
+        "insgesamt": "In Total",
+        "davon zweirädrige Kfz": "Two Wheel Vehicle",
+        "davon leichte vierrädrige Kfz": "Four Wheeler",
+        "darunter weibliche Halter": "female Vehicle",
+        "und zwar mit Allrad-antrieb": "All wheel drive",
+    },
+    inplace=True,
+)
+#Munster -> already the data is in english
+# Removing Unwanted Columns
+AutoScout24.drop(columns=["Regierungsbezirk", "Hubraum bis 1.399 cm³", "PKW-Dichte je 1.000 Einwohner"])
 
-####
+#Munster.drop(columns=["", ""])  # no need to remove column inn this
+
 
 # Land data to Database '''
 dataEngine = create_engine("sqlite:///MunsterAutoScout24.db")
